ef129490354026101edd8c4da6356db9f135dbdc
jiaominye <yejiaomin@gmail.com>
2023-11-26 18:35:53 -0800

    WheelOfFortune-React initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,55 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react';
+import Login from "./Login";
+import Game from './Game';
+import Ranking from './Ranking';
+import { Navigate, BrowserRouter as Router, Route,Routes } from 'react-router-dom';
+import { initializeApp } from 'firebase/app';
+import { getAuth } from 'firebase/auth';
+import { useAuthState } from 'react-firebase-hooks/auth';
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCCLKz_yEJS7oh59SQ3H6lMgKAGBGHAJ-0",
+  authDomain: "user-login-for-booklist.firebaseapp.com",
+  projectId: "user-login-for-booklist",
+  storageBucket: "user-login-for-booklist.appspot.com",
+  messagingSenderId: "453610822850",
+  appId: "1:453610822850:web:e39a09f475d5c910ca2f5b",
+  measurementId: "G-JJ741RB432"
+  };
+  
+  initializeApp(firebaseConfig);
 
 function App() {
+  const auth = getAuth();
+  const [user, loading, error] = useAuthState(auth);
+  if(loading){
+    return (<div>
+      <p>Initialising User...</p>
+    </div>);
+  }
+  if(error){
+    return (<div>
+      <p>auth errorr...</p>
+    </div>);
+  }
+  console.log(user);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <Router>
+      <Routes>
+        <Route
+          path="/login"
+          element={user ? <Navigate to="/" /> : <Login />}
+        />
+        <Route
+          path="/"
+          element={user ? <Game /> : <Navigate to="/login" />}
+        />
+        <Route
+          path="/ranking"
+          element={<Ranking/>}
+        />
+      </Routes>
+      </Router>
   );
 }
 
